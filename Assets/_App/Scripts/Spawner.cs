9de46aded7f707543327475502a4898c9a627036
DavidBobet <48726902+davidoulehibou@users.noreply.github.com>
2024-10-03 14:55:28 +0200

    v1 missiles

---
+++ Assets/_App/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public static Spawner Instance { get ; private set;}
+    void Awake()
+	{
+	if (Instance !=  null)
+		{
+		Destroy(Instance);
+		
+		}
+		else
+		{
+		Instance = this;
+		}
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
